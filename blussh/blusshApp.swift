e95b41130b4b060144a1ddf2868915da961642d0
Pablo Pusiol <pablo.pusiol@gmail.com>
2025-08-04 20:12:57 -0400

    Initial Commit

---
+++ blussh/blusshApp.swift
@@ -0,0 +1,17 @@
+//
+//  blusshApp.swift
+//  blussh
+//
+//  Created by Pablo Pusiol on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct blusshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
